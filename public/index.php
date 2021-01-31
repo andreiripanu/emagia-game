6f09cc187ce2c72eae9b4b1b304fe93828ec20da
Andrei Ripanu <andrei.ripanu93@gmail.com>
2021-01-31 14:05:26 +0200

    public folder added

--- public/index.php
+++ public/index.php
@@ -1,7 +1,14 @@
 <?php
 
+use Arcsym\Emagia\Battle;
+use Arcsym\Emagia\BattleOutputer;
 use Arcsym\Emagia\Game;
+use Arcsym\Emagia\Setting\BattleSetting;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new Game())->run();
+$battleSetting = new BattleSetting();
+$battleOutputer = new BattleOutputer($battleSetting);
+$battle = new Battle($battleSetting, $battleOutputer);
+
+(new Game($battle))->start();
