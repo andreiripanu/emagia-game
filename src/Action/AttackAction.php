2a55aa9bb2e10c42c9070402bf2970db4e1d4e0b
Andrei Ripanu <andrei.ripanu93@gmail.com>
2021-01-31 13:39:29 +0200

    actions for a battle added

---
+++ src/Action/AttackAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Arcsym\Emagia\Action;
+
+/*
+ * Represents attack action in a battle.
+ */
+final class AttackAction
+{
+  public const ACTION = 'attack';
+}
---
+++ src/Action/DefenceAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Arcsym\Emagia\Action;
+
+/*
+ * Represents defence action in a battle.
+ */
+final class DefenceAction
+{
+  public const ACTION = 'defence';
+}
